1187fc7d919b86b38514f7fda862d217710debc9
Josh Scott <joshscott82@gmail.com>
2022-12-22 21:00:18 -0500

    user

--- src/main/java/com/dinplan/java/user/UsersController.java
+++ src/main/java/com/dinplan/java/user/UsersController.java
@@ -1,5 +1,7 @@
 package com.dinplan.java.user;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -9,5 +11,13 @@ public class UsersController {
 	
 	@Autowired
 	private UserRepository userRepo;
+	
+	@GetMapping
+	public ResponseEntity<Iterable<User>> getUsers() {
+		Iterable<User> x = userRepo.findAll();
+		return new ResponseEntity<Iterable<User>>(x, HttpStatus.OK);
+	}
+	
+	
 
 }
