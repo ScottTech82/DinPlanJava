41eddb3233dfcd2070111aeaf403fab0bd858d99
Josh Scott <joshscott82@gmail.com>
2022-12-24 08:50:49 -0500

    usercont

--- src/main/java/com/dinplan/java/user/UserRepository.java
+++ src/main/java/com/dinplan/java/user/UserRepository.java
@@ -1,7 +1,11 @@
 package com.dinplan.java.user;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+	
+	Optional<User> findByCode(String code);
 
 }
