5817f25444ed855942293af1d2446bd7ce8988e8
Josh Scott <joshscott82@gmail.com>
2022-12-24 09:05:18 -0500

    adding meal

--- src/main/java/com/dinplan/java/meal/Meal.java
+++ src/main/java/com/dinplan/java/meal/Meal.java
@@ -1,5 +1,29 @@
 package com.dinplan.java.meal;
 
+import javax.persistence.*;
+
+
+@Entity(name="Meals")
+@Table
 public class Meal {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column(length=30, nullable=false)
+	private String name;
+	@Column(length=30, nullable=false)
+	private String type;
+	@Column(length=30, nullable=true)
+	private String description;
+	@Column(length=30, nullable=false)
+	private String cookType;
+
+	private int cookTemp;
+
+	private int cookTime;
+	
+	public Meal() {}
+	
 
 }
