6cd8c2ac871cb2397bce95388789f4deda1c9ed0
Josh Scott <joshscott82@gmail.com>
2022-12-27 15:51:29 -0500

    Ingrd interface

---
+++ src/main/java/com/dinplan/java/ingredient/IngredientRepository.java
@@ -0,0 +1,7 @@
+package com.dinplan.java.ingredient;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface IngredientRepository extends CrudRepository<Ingredient, Integer>{
+
+}
