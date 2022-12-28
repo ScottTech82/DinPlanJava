50ec427490e66563934641135ebbbb87ef176243
Josh Scott <joshscott82@gmail.com>
2022-12-28 11:49:28 -0500

    Ingdnt Contr

--- src/main/java/com/dinplan/java/ingredient/IngredientsController.java
+++ src/main/java/com/dinplan/java/ingredient/IngredientsController.java
@@ -1,6 +1,8 @@
 package com.dinplan.java.ingredient;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -10,6 +12,13 @@ public class IngredientsController {
 	
 	@Autowired
 	private IngredientRepository ingRepo;
+	
+	
+	@GetMapping
+	public ResponseEntity<Iterable<Ingredient>> getIngredients() {
+		Iterable<Ingredient> ingredient = ingRepo.findAll();
+		return new ResponseEntity<Iterable<Ingredient>>(ingredient, HttpStatus.OK);
+	}
 	
 	
 
